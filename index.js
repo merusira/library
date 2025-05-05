d41f6555543590ffffe140e08cb38a1f9193d512
merusira <127807821+merusira@users.noreply.github.com>
2025-05-04 17:53:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PRE_LOAD_MODULES = ['packet', 'library'];
 class Library{
 	constructor(dispatch, arg1) {
 		this.autism4 = true;
-		this.rival100 = true;
         this.mods = {};
 		this.command = dispatch.command;
 		this.cmd = this.command;
